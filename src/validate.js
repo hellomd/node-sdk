d4af346e9d76078f564f9a2b3a369f9471818453
Haz <hazdiego@gmail.com>
2018-01-16 17:21:18 -0200

    Add type and array values validators (#2)

--- src/validate.js
+++ src/validate.js
@@ -8,6 +8,28 @@ validate.validators.ref = function(value, options, key, attributes) {
   }
 }
 
+validate.validators.values = function(values, options) {
+  if (!values.map) return
+  const result = values.map(value => validate.single(value, options) || [])
+  return [].concat(...result)
+}
+
+validate.validators.type = function(value, type) {
+  if (!validate.isDefined(value)) return
+
+  const types = {
+    array: validate.isArray,
+    string: validate.isString,
+    boolean: validate.isBoolean,
+    number: validate.isNumber,
+    object: validate.isObject,
+  }
+
+  if (types[type] && !types[type](value)) {
+    return `value must be of type ${type}`
+  }
+}
+
 validate.extend(validate.validators.datetime, {
   parse: value => moment(value, moment.iso8601, true).utc().toDate(),
   format: (value, options) =>  moment.utc(value).format(options.dateOnly ? 'YYYY-MM-DD' : moment.iso8601)
