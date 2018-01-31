bdd46470a82f48249da0c943103ae9f0076fd968
Diego Haz <hazdiego@gmail.com>
2018-01-31 16:53:16 -0200

    Add bool filter

--- src/filters.js
+++ src/filters.js
@@ -1,5 +1,9 @@
+const convertStringToBoolean = value => (
+  typeof value === 'undefined' || ['0', 'false'].includes(value) ? false : true
+)
+
 const eq = (ctx, queryKey, dbKey = queryKey, transform = v => v) => {
-  if (ctx.query[queryKey]) {
+  if (typeof ctx.query[queryKey] !== 'undefined') {
     return {
       [dbKey]: transform(ctx.query[queryKey])
     }
@@ -7,30 +11,33 @@ const eq = (ctx, queryKey, dbKey = queryKey, transform = v => v) => {
   return {}
 }
 
-const ne = (ctx, queryKey, dbKey = queryKey, transform = v => v) => {
-  return eq(ctx, queryKey, dbKey, v => ({ "$ne": transform(v) }))
-}
+const ne = (ctx, queryKey, dbKey = queryKey, transform = v => v) => (
+  eq(ctx, queryKey, dbKey, v => ({ "$ne": transform(v) }))
+)
 
-const regExp = (ctx, queryKey, dbKey = queryKey, modifiers = 'i') => {
-  return eq(ctx, queryKey, dbKey, value => new RegExp(value, modifiers))
-}
+const bool = (ctx, queryKey, dbKey = queryKey, transform = v => v) => (
+  eq(ctx, queryKey, dbKey, v => transform(convertStringToBoolean(v)))
+)
+
+const regExp = (ctx, queryKey, dbKey = queryKey, modifiers = 'i') => (
+  eq(ctx, queryKey, dbKey, value => new RegExp(value, modifiers))
+)
 
 /**
  * @apiDefine PublishedFilter
  * @apiParam {Boolean} [published] Published filter e.g. /?published=true
  */
-const published = ctx => {
-  return eq(ctx, 'published', 'publishedAt', v => {
-    if (v === 'false') {
-      return null
-    }
+const published = ctx => eq(ctx, 'published', 'publishedAt', v => {
+  if (convertStringToBoolean(v)) {
     return { $ne: null }
-  })
-}
+  }
+  return null
+})
 
 module.exports = {
   eq,
   ne,
+  bool,
   regExp,
   published,
 }
