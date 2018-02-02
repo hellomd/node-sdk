2b3155dd3f045b809cc84ba13cacb70fc3cdba63
Diego Haz <hazdiego@gmail.com>
2018-02-02 13:58:03 -0200

    Add null utils

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,37 @@
+const moment = require('moment')
+const { ObjectId } = require('mongodb')
+
+const nullify = o =>
+  Object.entries(o).reduce(
+    (acc, [k, v]) => ({ ...acc, [k]: v === undefined ? null : v }),
+    {},
+  )
+
+const nullOrDateString = v => v && v.toISOString()
+
+const nullOrDate = v =>
+  v &&
+  moment(v, moment.iso8601, true)
+    .utc()
+    .toDate()
+
+const nullOrObjectId = v => v && ObjectId(v)
+
+const nullOrToString = v => v && v.toString()
+
 const times = (n, fn) => Array.from(Array(n)).map((_, i) => fn(i))
-const sample = arr => arr[randomInt(arr.length - 1)]
+
 const randomInt = (max = 100) => Math.floor(Math.random() * Math.floor(max))
 
+const sample = arr => arr[randomInt(arr.length - 1)]
+
 module.exports = {
+  nullify,
+  nullOrDateString,
+  nullOrDate,
+  nullOrObjectId,
+  nullOrToString,
   times,
-  sample,
   randomInt,
+  sample,
 }
