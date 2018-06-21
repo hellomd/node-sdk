39ad863373746b614c505e1a193882eedf098fb7
Marcello Milhomem Albuquerque <marcello.m.albuquerque@gmail.com>
2018-06-21 11:55:19 -0300

    Api helpers

---
+++ src/api.js
@@ -0,0 +1,83 @@
+const { axios } = require('./axios')
+
+const serviceTokenHeader = (ctx, args) => ({
+  'Content-Type': 'application/json',
+  Authorization: `bearer ${ctx.state.serviceToken}`,
+})
+
+const buildKoaEndpoint = ctx => def => {
+  return async (ctx, args) => {
+    try {
+      return buildEndpoint(ctx)(args)
+    } catch (err) {
+      ctx.throw(err.code, err.message)
+    }
+  }
+}
+
+const defaultError = message => ({
+  code: '500',
+  message,
+})
+
+const newError = ({ message, ...obj }) => Object.assign(new Error(message), obj)
+
+const buildEndpoint = ctx => def => {
+  const {
+    method = 'GET',
+    url: urlFn,
+    data: dataFn,
+    transform = x => x,
+    headers = [serviceTokenHeader],
+    maxRetries = 3,
+    errors = {},
+  } = def
+
+  return async args => {
+    const url = typeof urlFn === 'function' ? urlFn(ctx, args) : urlFn
+    const data = typeof dataFn === 'function' ? dataFn(ctx, args) : dataFn
+    for (let i = 0; i <= maxRetries; i++) {
+      try {
+        const { data: results } = await axios({
+          method,
+          url,
+          data,
+          headers: headers.reduce(
+            (prev, curr) =>
+              Object.assign(
+                prev,
+                typeof curr === 'function' ? curr(ctx, args) : curr,
+              ),
+            {},
+          ),
+        })
+        return transform(results)
+      } catch (err) {
+        const resCode = err.reponse ? err.response.status : 500
+        if (resCode < 500 || i === maxRetries) {
+          throw newError(errors[resCode.toString()] || defaultError(err))
+        }
+      }
+    }
+  }
+}
+
+const buildKoaApi = def => ctx =>
+  Object.entries(def).reduce(
+    (prev, [k, v]) => ({ ...prev, [k]: buildKoaEndpoint(ctx)(v) }),
+    {},
+  )
+
+const buildApi = def => ctx =>
+  Object.entries(def).reduce(
+    (prev, [k, v]) => ({ ...prev, [k]: buildEndpoint(ctx)(v) }),
+    {},
+  )
+
+module.exports = {
+  buildApi,
+  buildEndpoint,
+  buildKoaApi,
+  buildKoaEndpoint,
+  serviceTokenHeader,
+}
