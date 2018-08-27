262a22cef6fa6514612f05d93bef0669959fdd84
Marcello Albuquerque <marcello.m.albuquerque@gmail.com>
2018-08-27 09:14:35 -0300

    Updated mongodb

--- src/mongo/mapCollections.js
+++ src/mongo/mapCollections.js
@@ -1,9 +1,10 @@
 require('mongodb')
 
-module.exports = (db, collections) => Object.entries(collections).reduce(
-  (acc, [k, v]) => ({
-    ...acc,
-    [k]: db.collection(v),
-  }),
-  {},
-)
+module.exports = (db, collections) =>
+  Object.entries(collections).reduce(
+    (acc, [k, v]) => ({
+      ...acc,
+      [k]: db.db().collection(v),
+    }),
+    {},
+  )
