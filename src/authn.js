7d1769b55a100e61ef20e12427121dbc9980f04a
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-02-12 13:37:29 -0200

    add getAnonymousToken fn

--- src/authn.js
+++ src/authn.js
@@ -1,6 +1,12 @@
 const { promisify } = require('util')
 const { sign } = require('jsonwebtoken')
 
+const TOKEN_KIND = {
+  ANONYMOUS_USER: 'anonymous_user',
+  LOGGED_USER: 'logged_user',
+  SERVICE: 'service',
+}
+
 const jwtToken = (claims, secret) =>
   promisify(sign)(claims, secret, { algorithm: 'HS256' })
 
@@ -19,6 +25,7 @@ const getToken = (
 ) => {
   if (isService && process.env.ENV === 'test') return 'serviceToken'
 
+  const kind = isService ? TOKEN_KIND.SERVICE : TOKEN_KIND.LOGGED
   const exp = Math.round(expireDate(expiresIn).getTime() / 1000)
   const data = id
     ? {
@@ -26,9 +33,20 @@ const getToken = (
         email,
         isService,
         exp,
+        kind,
       }
-    : { appName, isService, exp }
+    : { appName, isService, exp, kind }
   return jwtToken(data, process.env.SECRET)
+}
+
+const getAnonymousToken = id => {
+  return jwtToken(
+    {
+      id,
+      kind: TOKEN_KIND.ANONYMOUS,
+    },
+    process.env.SECRET,
+  )
 }
 
 const serviceTokenMiddleware = async (ctx, next) => {
@@ -39,5 +57,7 @@ const serviceTokenMiddleware = async (ctx, next) => {
 
 module.exports = {
   getToken,
+  getAnonymousToken,
   serviceTokenMiddleware,
+  TOKEN_KIND,
 }
