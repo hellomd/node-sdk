ea9df379b14b93208a3c1f831628b7decda6c23a
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-02-13 17:19:00 -0200

    allow authn test helper to receive a rest param to add additional properties to the jwt

--- src/testHelpers.js
+++ src/testHelpers.js
@@ -1,8 +1,11 @@
 const R = require('ramda')
 const jwt = require('jsonwebtoken')
 
-const authn = id => req => {
-  req.set('Authorization', `bearer ${jwt.sign({ id }, process.env.SECRET)}`)
+const authn = (id, rest = {}) => req => {
+  req.set(
+    'Authorization',
+    `bearer ${jwt.sign({ id, ...rest }, process.env.SECRET)}`,
+  )
 }
 
 const omitId = R.omit(['_id', 'id'])
