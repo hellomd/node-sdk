36204569262cad722bfcb6cfbd55fd803ab5c8ff
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-02-13 17:21:50 -0200

    set kind to user by default on authn test helper jwt

--- src/testHelpers.js
+++ src/testHelpers.js
@@ -1,10 +1,15 @@
 const R = require('ramda')
 const jwt = require('jsonwebtoken')
 
+const { TOKEN_KIND } = require('./authn')
+
 const authn = (id, rest = {}) => req => {
   req.set(
     'Authorization',
-    `bearer ${jwt.sign({ id, ...rest }, process.env.SECRET)}`,
+    `bearer ${jwt.sign(
+      { id, kind: TOKEN_KIND.USER, ...rest },
+      process.env.SECRET,
+    )}`,
   )
 }
 
