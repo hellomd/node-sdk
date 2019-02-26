3f2f0219b4c7af846dce38d7bb91829db7648dbf
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-02-26 11:11:10 -0300

    add toBeResourceRef jest matcher

--- src/jestUtils/customMatchers.js
+++ src/jestUtils/customMatchers.js
@@ -16,6 +16,49 @@ if (isJestRunning) {
       }
     },
 
+    // strict means that if we are testing not.toBeResourceRef it will still check if it's a valid ref
+    toBeResourceRef(received, resourceKind, resourceId) {
+      const isString = typeof received === 'string'
+      const pieces = isString ? received.split(':') : []
+
+      const isValid = pieces.length === 2 && Object.isValid(pieces[1])
+
+      if (
+        typeof resourceKind === 'undefined' &&
+        typeof resourceId === 'undefined'
+      ) {
+        return {
+          message: () =>
+            isValid
+              ? `expected ${received} not to be a valid resource ref string`
+              : `expected ${received} to be a valid resource ref string`,
+          pass: isValid,
+        }
+      }
+
+      let pass = isValid
+      let message = pass
+        ? `expected ${received} not to be a valid resource ref string`
+        : `expected ${received} to be a valid resource ref string`
+
+      const [receivedResourceKind, receivedResourceId] = pieces
+
+      if (resourceKind) {
+        pass = pass && receivedResourceKind === resourceKind
+        message = `${message}, with resource kind of ${resourceKind}`
+      }
+
+      if (resourceId) {
+        pass = pass && receivedResourceId === resourceId
+        message = `${message} and resource id of ${resourceId}`
+      }
+
+      return {
+        message: () => message,
+        pass,
+      }
+    },
+
     toBeISOString(received) {
       const pass = moment(received, 'YYYY-MM-DDTHH:mm:ss.SSSZ', true).isValid()
       return {
