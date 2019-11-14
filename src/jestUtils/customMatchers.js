9012a1d8dfd0b62af1682943eebac4002e3d97f7
Jonathan Cardoso Machado <me@jonathancardoso.com>
2019-11-14 16:38:05 -0300

    add matchers for uuid testing on jest utils

--- src/jestUtils/customMatchers.js
+++ src/jestUtils/customMatchers.js
@@ -10,6 +10,9 @@ const moment = require('moment')
 
 const isJestRunning = require('./isJestRunning')
 
+const uuidV1Regex = /^[0-9A-F]{8}-[0-9A-F]{4}-1[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+const uuidV4Regex = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i
+
 if (isJestRunning) {
   expect.extend({
     toBeObjectId(received) {
@@ -22,7 +25,26 @@ if (isJestRunning) {
         pass,
       }
     },
-
+    toBeUuidV1(received) {
+      const pass = !!received && uuidV1Regex.test(received)
+      return {
+        message: () =>
+          pass
+            ? `expected ${received} not to be a valid uuid v1`
+            : `expected ${received} to be a valid uuid v1`,
+        pass,
+      }
+    },
+    toBeUuidV4(received) {
+      const pass = !!received && uuidV4Regex.test(received)
+      return {
+        message: () =>
+          pass
+            ? `expected ${received} not to be a valid uuid v4`
+            : `expected ${received} to be a valid uuid v4`,
+        pass,
+      }
+    },
     // strict means that if we are testing not.toBeResourceRef it will still check if it's a valid ref
     toBeResourceRef(received, resourceKind, resourceId) {
       const isString = typeof received === 'string'
