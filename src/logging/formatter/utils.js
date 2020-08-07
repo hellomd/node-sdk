45b855e08a8c8b588a8f66d8fe00a349a52a9b7d
Jonathan Cardoso Machado <me@jonathancardoso.com>
2020-08-07 16:30:49 -0300

    logging: add bigint handling to jsonReplacer

--- src/logging/formatter/utils.js
+++ src/logging/formatter/utils.js
@@ -1,4 +1,5 @@
 function jsonReplacer(key, value) {
+  if (typeof value === 'bigint') return value.toString(10)
   if (value instanceof Buffer) return value.toString('base64')
   if (value instanceof Error)
     return {
